3e3ef8c80cb5329256a9e2991f686f41a94b19e1
Ryan Harris <ryan.harris@okbu.edu>
2024-04-23 22:49:54 -0500

    NOW i have added code to read a csv file lol

--- csvtest.py
+++ csvtest.py
@@ -2,16 +2,30 @@ from genesys import Genesys
 import serial
 import csv
 import pandas as pd
+import time
 
+with open('computingdata.csv', 'r', newline='') as csvfile:
+    header = csvfile.readline().strip()
+
+with open ('computingdata.csv', 'w', newline='') as csvfile:
+    csvfile.write(header + '\n')
+    
 ser = serial.Serial('COM3', 9600)
 
-with open('computingdata.csv', 'w', newline='') as csvfile:
+with open('computingdata.csv', 'a', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["Time", "Absorbance"])
-    while True:
+    start_time = time.time()
+    while time.time() - start_time < 15:
         data = ser.readline().decode('utf-8').rstrip()
         if data:
             time, absorbance = data.split(',')
             writer.writerow([time, absorbance])
+        time.sleep(1)
 
 ser.close()
+
+for i in range(15):
+    df = pd.read_csv('computingdata.csv')
+    print(df)
+    time.sleep(1)
