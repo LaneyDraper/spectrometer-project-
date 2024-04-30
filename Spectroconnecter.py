282da4bdc956483a05e308b2f4c77e6686aae47f
Nathan Robbins <robbins1303@gmail.com>
2024-04-30 15:07:12 -0500

    made code to communicate with spectrometer

--- Spectroconnecter.py
+++ Spectroconnecter.py
@@ -1,8 +1,14 @@
 from genesys import Genesys
+import time
 
+def sleep(seconds):
+    time.sleep(seconds)
+    
 spectrometer = Genesys("COM3")
 spectrometer.wavelength(wl=500)
 spectrometer.beep()
 spectrometer.blank()
 spectrometer.beep(times=3)
+time.sleep(6)
+spectrometer.beep()
 spectrometer.reading()
