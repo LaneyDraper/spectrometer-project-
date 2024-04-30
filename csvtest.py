86c9daa0344ecbb589a0e9d5b4e55d67e8ff6fbb
Ryan Harris <ryan.harris@okbu.edu>
2024-04-30 13:08:44 -0500

    renamed

--- csvtest.py
+++ csvtest.py
@@ -4,20 +4,20 @@ import csv
 import pandas as pd
 import time
 
-with open('computingdata.csv', 'r', newline='') as csvfile:
+with open("computingdata.csv", "r", newline='') as csvfile:
     header = csvfile.readline().strip()
 
-with open ('computingdata.csv', 'w', newline='') as csvfile:
-    csvfile.write(header + '\n')
+with open ("computingdata.csv", "w", newline='') as csvfile:
+    csvfile.write(header + "\n")
     
-ser = serial.Serial('COM3', 9600)
+ser = serial.Serial("COM3", 9600)
 
-with open('computingdata.csv', 'a', newline='') as csvfile:
+with open("computingdata.csv", "a", newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["Time", "Absorbance"])
     start_time = time.time()
     while time.time() - start_time < 30:
-        data = ser.readline().decode('utf-8').rstrip()
+        data = ser.readline().decode("utf-8").rstrip()
         if data:
             time, absorbance = data.split(',')
             writer.writerow([time, absorbance])
@@ -26,6 +26,6 @@ with open('computingdata.csv', 'a', newline='') as csvfile:
 ser.close()
 
 for i in range(30):
-    df = pd.read_csv('computingdata.csv')
+    df = pd.read_csv("computingdata.csv")
     print(df)
     time.sleep(1)
