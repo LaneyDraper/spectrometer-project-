519f113069ae9f7bf93573a802b9053c771b75a7
Ryan Harris <ryan.harris@okbu.edu>
2024-04-30 15:17:28 -0500

    merged my file with nathan's

--- Spectroconnecter.py
+++ Spectroconnecter.py
@@ -1,5 +1,8 @@
 from genesys import Genesys
 import time
+import csv
+import pandas as pd
+import serial
 
 def sleep(seconds):
     time.sleep(seconds)
@@ -12,3 +15,30 @@ spectrometer.beep(times=3)
 time.sleep(6)
 spectrometer.beep()
 spectrometer.reading()
+
+start_time = time.time()
+
+time_data = []
+absorbance_data = []
+
+def read_spectrometer_data():
+    ser = spectrometer
+    ser.reading()
+    response = ser.readline().decode('utf-8').strip()
+    absorbance = float(response)
+
+    return absorbance
+
+start_time = time.time()
+
+while time.time() - start_time < 10:
+    absorbance = spectrometer.reading() 
+    current_time = time.time() - start_time
+    print(current_time)
+    time_data.append(current_time)
+    absorbance_data.append(absorbance)
+    time.sleep(0.01)
+df = pd.DataFrame({'Time': time_data, 'Absorbance': absorbance_data})
+df.to_csv('computingdata.csv', index=False)
+
+spectrometer.beep()
